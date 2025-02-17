e8284902f81b6e7353e671a7296a36b4e6e9851d
MagretBro <magretbro@gmail.com>
2025-02-17 21:10:27 +0700

    проведен refactor webpack.config.ts выделены отдельные сущности для читабельности и лучшей управляемости проектом

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,68 +1,33 @@
 // const path = require('path')
-import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from 'path';
 import webpack from 'webpack';
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { buildWebpack } from "./config/build/buildWebpack";
+import { BuildMode, BuildPaths, BuildPlatform } from './config/build/types/types';
 
 type Mode = 'production' | 'development';
 interface EnvVariables {
-  mode: Mode;
+  mode: BuildMode;
   port: number;
+  analyzer?: boolean;
+  platform?: BuildPlatform
 }
 
 export default (env: EnvVariables) => {
 
-    const isDev = env.mode === 'development';
+  const paths: BuildPaths = {
+    output: path.resolve(__dirname, 'build'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    public: path.resolve(__dirname, 'public'),
+    src: path.resolve(__dirname, 'src'),
+  }
 
-    const config: webpack.Configuration =
-    {
+    const config: webpack.Configuration = buildWebpack({
+      port: env.port ?? 3000,
         mode: env.mode ?? 'development',
-        entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        output: {
-            path: path.resolve(__dirname, 'build'),
-            filename: '[name].[contenthash].js',
-            clean: true
-        },
-        plugins: [
-            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
-            isDev && new webpack.ProgressPlugin(), // показывает прогресс, но тормозит
-            new MiniCssExtractPlugin(
-              {
-                filename: "css/[name].[contenthash:8].css",
-                chunkFilename: "css/name].[contenthash:8].css",
-              
-              }
-            ),
-          ].filter(Boolean),
-        module: {
-            rules: [
-              {
-                test: /\.s[ac]ss$/i,
-                use: [
-                  MiniCssExtractPlugin.loader,
-                 
-                  // Translates CSS into CommonJS
-                  "css-loader",
-                  // Compiles Sass to CSS
-                  "sass-loader",
-                ],
-              },
-              {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-              },
-            ],
-          },
-          resolve: {
-            extensions: ['.tsx', '.ts', '.js'],
-          },
-            devtool:  isDev && 'inline-source-map',
-            devServer: isDev ? {
-            port: env.port ?? 3000,
-            open: true
-          } : undefined
-    }
+        paths,
+        analyzer: env.analyzer,
+        platform: env.platform ?? 'desktop'
+    });
     return config
 };
